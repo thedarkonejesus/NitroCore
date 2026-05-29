14a23be662a1f2dd77a99e48e94559fd0b55ce7d
JesusTheHacker <skylerhend@gmail.com>
2026-05-29 02:45:24 -0500

    init: Add NitroCore package initialization

---
+++ NitroCore/source/__init__.py
@@ -0,0 +1,7 @@
+"""NitroCore - Windows System Optimizer"""
+
+__version__ = "1.0.0"
+__author__ = "NitroCore Team"
+__description__ = "Professional Windows system optimization utility"
+
+__all__ = ["__version__", "__author__", "__description__"]
